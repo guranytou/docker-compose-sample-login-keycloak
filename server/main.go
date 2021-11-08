a325a8f5516cd1febf5a439a2d47f9616b2eb4e9
guranytou <guranytou@gmail.com>
2021-11-08 20:34:50 +0900

    一旦fix

--- server/main.go
+++ server/main.go
@@ -19,15 +19,13 @@ var oauth2Config *oauth2.Config
 func getConfig() (*oauth2.Config, *oidc.Provider) {
 	once.Do(func() {
 		var err error
-		// ここにissuer情報を設定
 		provider, err = oidc.NewProvider(context.Background(), "http://localhost:8080/auth/realms/master")
 		if err != nil {
 			panic(err)
 		}
 		oauth2Config = &oauth2.Config{
-			// ここにクライアントIDとクライアントシークレットを設定
 			ClientID:     "testapp",
-			ClientSecret: "e1a8b706-a9ac-4dd7-84fc-1d2513a0763e",
+			ClientSecret: "Client Credential",
 			Endpoint:     provider.Endpoint(),
 			Scopes:       []string{oidc.ScopeOpenID},
 			RedirectURL:  "http://localhost:8000/callback",
@@ -37,9 +35,7 @@ func getConfig() (*oauth2.Config, *oidc.Provider) {
 }
 
 func main() {
-	// 認証で保護したいページ。ログインしていなければKeycloakのOpenID Connect認証ページに飛ばす
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		// クッキーがない時はリダイレクト
 		if _, err := r.Cookie("Authorization"); err != nil {
 			config, _ := getConfig()
 			url := config.AuthCodeURL("")
@@ -49,8 +45,6 @@ func main() {
 		io.WriteString(w, "login success")
 	})
 
-	// OpenID Connectの認証が終わった時に呼ばれるハンドラ
-	// もろもろトークンを取り出したりした後に、クッキーを設定して元のページに飛ばす
 	http.HandleFunc("/callback", func(w http.ResponseWriter, r *http.Request) {
 		config, provider := getConfig()
 		if err := r.ParseForm(); err != nil {
@@ -78,8 +72,7 @@ func main() {
 			http.Error(w, "id token verify error", http.StatusInternalServerError)
 			return
 		}
-		// IDトークンのクレームをとりあえずダンプ
-		// アプリで必要なものはセッションストレージに入れておくと良いでしょう
+
 		idTokenClaims := map[string]interface{}{}
 		if err := idToken.Claims(&idTokenClaims); err != nil {
 			http.Error(w, err.Error(), http.StatusInternalServerError)
@@ -88,10 +81,11 @@ func main() {
 		log.Printf("%#v", idTokenClaims)
 		http.SetCookie(w, &http.Cookie{
 			Name:  "Authorization",
-			Value: "Bearer " + rawIDToken, // 行儀が悪いので真似しないねで
+			Value: "Bearer " + rawIDToken,
 			Path:  "/",
 		})
 		http.Redirect(w, r, "/", http.StatusFound)
 	})
+	log.Println("start http server :8080")
 	log.Println(http.ListenAndServe(":8000", nil))
 }
--- terraform/main.tf
+++ terraform/main.tf
@@ -30,12 +30,19 @@ resource "keycloak_realm" "master" {
 
 resource "keycloak_openid_client" "openid_client" {
   realm_id            = keycloak_realm.master.id
-  client_id           = "test-client"
-
+  client_id           = "testapp"
   name                = "test client"
   enabled             = true
-
   access_type         = "CONFIDENTIAL"
+  standard_flow_enabled = "true"
+  implicit_flow_enabled = "true"
+
+  root_url = "http://localhost:8000"
+  admin_url = "http://localhost:8000"
+  web_origins = ["http://localhost:8000"]
+  valid_redirect_uris = [
+    "http://localhost:8000/*"
+  ]
 
   login_theme = "keycloak"
 }
